dd604e9e6ff85dd87fe2c70f73276f61ed8b8bd3
thounny <thounnykeo@gmail.com>
2024-10-28 14:13:49 -0400

    .join()

--- elements/elements.js
+++ elements/elements.js
@@ -14,3 +14,16 @@ function getMonster(m, i) {
 
 // .map() transforms INPUT array into an OUTPUT array
 console.log(monsters.map(getMonster).join(`\n`));
+
+const numbers = [1, 2, 3, 4];
+console.log(numbers);
+
+// separator
+const separator = "."; // GLUE
+console.log(numbers.join(separator));
+const text = "255.255.0.1";
+
+const delimiter_AKA_splitter = ".";
+const parsed = text.split(delimiter_AKA_splitter);
+console.log(parsed);
+console.log(parsed.join("-"));
