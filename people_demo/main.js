14b615ed1130568abc2491e8c42d93e043afc704
thounny <thounnykeo@gmail.com>
2024-10-28 15:03:14 -0400

    updated js to show three ways for map()

--- people_demo/main.js
+++ people_demo/main.js
@@ -9,7 +9,16 @@
 // ];
 
 document.addEventListener("DOMContentLoaded", () => {
+  // one way
   peopleSelect.innerHTML += people
     .map((p) => `<option>${p.email}</option>`)
     .join("");
+
+  // second way
+  people
+    .map((p) => new Option(p.email))
+    .forEach((o) => peopleSelect2.appendChild(o));
+
+  // third way (better / practical for map)
+  people.map((p) => peopleSelect3.appendChild(new Option(p.email)));
 }); // end load
